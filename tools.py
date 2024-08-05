450eae8497b205510c55843c83427877f4bd5ce0
paumazcu <99pau99@gmail.com>
2024-08-06 01:12:22 +0200

    crec que funciona pero a vegades falla infinitament no sé perquè

--- tools.py
+++ tools.py
@@ -243,28 +243,3 @@ def assign_names_to_tasks(random_seed):
 
     return week, nits_desp_count_per_moni, meals_count_per_moni, tl_count_per_moni, total_count_per_moni
 
-
-def run():
-    success = False
-    attempts = 0
-    max_attempts = 100
-
-    while not success and attempts < max_attempts:
-        attempts += 1
-        try:
-            random_seed = time.time() + attempts
-            prova, nits_counts, meals_counts, tl_counts, total_counts = tools.assign_names_to_tasks(random_seed)
-            success = True
-            for d in prova.items():
-                print(d)
-
-            print("\n NITS\n", nits_counts)
-            print("\n MEALS\n", meals_counts)
-            print("\n TL\n", tl_counts)
-            print("\n TOTALS\n", total_counts)
-
-        except (ValueError, IndexError) as e:
-            print(f"Attempt {attempts} failed: {e}. Trying again...")
-
-    if not success:
-        print("Failed to complete the algorithm after maximum attempts.")
